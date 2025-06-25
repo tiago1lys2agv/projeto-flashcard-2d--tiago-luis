fe671d4ddefc9b21073cb214903fa858bc21deae
tiago1lys2agv <tiagolu0907@gmail.com>
2025-06-25 15:58:15 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,19 +1,31 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Lingua inglesa',
+    'Existe preconceito de sotaque?',
+    'Sim existe, como os povos de origem africana fala por exemplo'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Lingua inglesa',
+    'Como se diz tanto faz em inglês?',
+    'Se diz whaterever'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Lingua inglesa',
+    'Por que se deve estudar inglês?',
+    'Por conta de ser uma das linguagens que se abre mais porta'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como o inglês impacta na nossa vida?',
+    'De forma que maior parte do conhecimento, entreterimento e cultura terá que ter uma "tradução" em inglês'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    '',
+    'Oi em ingles é HI (RAI)'
 )
 
 criaCartao(
